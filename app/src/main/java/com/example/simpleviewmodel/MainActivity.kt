fa0379e9cec41f7a28425dce4ff46669624c51ce
satriomuslim <satriomuslim2627@gmail.com>
2023-06-09 15:46:09 +0700

    end commit

--- app/src/main/java/com/example/simpleviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/simpleviewmodel/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.simpleviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.simpleviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+//  private lateinit var viewModel: MainViewModel
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//      viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Still Empty"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Still Empty"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Still Empty"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+
+            }
+
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
     }
 }
---
+++ app/src/main/java/com/example/simpleviewmodel/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.simpleviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
+
+}
